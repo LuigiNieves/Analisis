d0087b18b27499e39507e79c6aa8e144d3597373
luigi nieves <luigi_nieves@hotmail.com>
2023-11-23 05:53:44 -0500

    la vieja leidy

--- functions/Interpolation.py
+++ functions/Interpolation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
+from PIL import Image, ImageTk
 
 X=sp.symbols('X')
 
@@ -43,3 +44,70 @@ def lagrange(xdata,ydata):
     P = P + T*ydata[i]
   print('El polinomio interpolante es : P(X)=' ,P)
   return sp.lambdify(X,P)
+
+def graficas9(x,y):
+  plt.subplots_adjust(left=None, bottom=None,right=None,top=None,wspace=None,hspace=0.5)
+  plt.figure(figsize=(16,12))
+
+  plt.subplot(331)
+  plt.plot(x,y,'bo')
+  plt.grid()
+  plt.legend()
+  plt.title('Datos observados')
+
+
+  plt.subplot(332)
+  plt.plot(x**2,y,'m*')
+  plt.grid()
+  plt.legend()
+  plt.title('x^2')
+
+
+  plt.subplot(333)
+  plt.plot(x**3,y,'cd')
+  plt.grid()
+  plt.legend()
+  plt.title('x^3')
+
+
+  plt.subplot(334)
+  plt.plot(x**(1/2),y,'yp')
+  plt.grid()
+  plt.legend()
+  plt.title('sqrt(x)')
+
+
+
+  plt.subplot(335)
+  plt.plot(np.log(x),y,'bv')
+  plt.grid()
+  plt.legend()
+  plt.title('log(x)')
+
+
+  plt.subplot(336)
+  plt.plot(x,np.log(y),'g*')
+  plt.grid()
+  plt.legend()
+  plt.title('log(y)')
+
+
+  plt.subplot(337)
+  plt.plot(x,y**2,'rp')
+  plt.grid()
+  plt.legend()
+  plt.title('sqrt(y)')
+
+
+  plt.subplot(338)
+  plt.plot(np.log(x),np.log(y),'bs')
+  plt.grid()
+  plt.legend()
+  plt.title('log(x) y log(y)')
+
+
+  plt.subplot(339)
+  plt.plot(x,1/y**(1/2),'y*')
+  plt.grid()
+  plt.legend()
+  plt.title('1/sqrt(y)')
--- windows/IInterpolation.py
+++ windows/IInterpolation.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from functions.Interpolation import *
+from sympy import symbols,lambdify,exp,log
+from tkinter import ttk
 from PIL import Image, ImageTk
 
 
@@ -27,37 +29,39 @@ class AInterpolation(tk.Frame):
     top = tk.Toplevel(self)
     top.title("Tabla de datos")
 
-    imagen_tk = ImageTk.PhotoImage(grafica)
-
+    imagen = Image.open('img.png')
+    imagen_tk = ImageTk.PhotoImage(imagen)
     label_imagen = tk.Label(top, image=imagen_tk)
     label_imagen.pack()
-
     
-class CPsimple(AInterpolation):
+
+class CMinimos(AInterpolation):
   def __init__(self, parent, controller, orden=0, cuadros=0):
-    cuadros = ["xdata","tdata"]
+    cuadros = ["xdata","ydata"]
     super().__init__(parent, controller, orden, cuadros)
     ultima_fila = self.grid_size()[1] - 1
     
     self.result = tk.Label(self, text="" , width=65, wraplength=200)
     self.result.grid(row=ultima_fila+1, column=1, padx=1, pady=1,sticky='nsew' )
     
-    execute = tk.Button(self,text="Ejecutar",command=lambda:self.solve_Psimple())  
-    execute.grid(row=ultima_fila+2, column=0, padx=2, pady=5) 
+    execute = tk.Button(self,text="Ejecutar",command=lambda:self.solve_minimos())  
+    execute.grid(row=ultima_fila+2, column=0, padx=2, pady=5)
     
-  def graficar(self,t,P):
-    # P=np.array(P)
-    # plt.subplot(121)
-    # plt.plot(t,P[:,0])
-    # plt.grid()
-    # # plt.xlim(9.4, 9.6)
-    # plt.xlabel('tiempo')
-    # plt.ylabel('posicion')
-    # plt.subplot(122)
-    # plt.plot(t,P[:,1])
-    # plt.xlabel('tiempo')
-    # plt.xlabel('posicion')
-    # plt.grid(True)
+    xdata = [float(i) for i in self.entries[0].get().split(' ') if self.isNumeric(i)]
+    ydata = [float(i) for i in self.entries[1].get().split(' ') if self.isNumeric(i)]
+    
+    grafica=self.graficar(xdata,ydata)
+    self.show_result(grafica)
+  
+  def isNumeric(self,s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False   
+    
+  def graficar(self,xdata,ydata):
+  
     x = np.linspace(0, 10, 100)
     y = np.sin(x)
 
@@ -68,14 +72,35 @@ class CPsimple(AInterpolation):
     plt.ylabel('Eje Y')
     plt.grid(True)
         
-    plt.savefig('img')
+    plt.savefig('img.png')
     plt.close()
-    
-    imagen = Image.open('imgn')
-    imagen_tk = ImageTk.PhotoImage(imagen)
+   
 
-    return imagen
-  
+  def solve_minimo(self): 
+    try:
+      xdata = [float(i) for i in self.entries[0].get().split(' ') if self.isNumeric(i)]
+      ydata = [float(i) for i in self.entries[1].get().split(' ') if self.isNumeric(i)]
+    
+      a0,a1 = minimos_cuadrados(xdata,ydata)  
+    except Exception as e:
+      print(e) 
+
+    
+class CPsimple(AInterpolation):
+  def __init__(self, parent, controller, orden=0, cuadros=0):
+    cuadros = ["xdata","ydata"]
+    super().__init__(parent, controller, orden, cuadros)
+    ultima_fila = self.grid_size()[1] - 1
+    
+    self.result = tk.Label(self, text="" , width=65, wraplength=200)
+    self.result.grid(row=ultima_fila+1, column=1, padx=1, pady=1,sticky='nsew' )
+    
+    execute = tk.Button(self,text="Ejecutar",command=lambda:self.solve_Psimple())  
+    execute.grid(row=ultima_fila+2, column=0, padx=2, pady=5) 
+    
+    
+    
+
   def isNumeric(self,s):
     try:
         float(s)
@@ -90,6 +115,5 @@ class CPsimple(AInterpolation):
     
       polinomioS,P = p_simple(xdata,ydata)  
       self.result.config(text=P)
-      # ultima_fila = self.grid_size()[1] - 1
     except Exception as e:
       print(e)   
